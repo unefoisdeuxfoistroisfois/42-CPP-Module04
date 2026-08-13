864a4ed590e4ff7057bfdb2e2c759fe265a2508a
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-08-13 21:05:25 +0200

    main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,4 +1,29 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+
 int main(){
 
-    return (0);
+  // main de l'ecole 
+  
+  std::cout << "=== Constructor === " << std::endl;
+  const Animal* meta = new Animal();
+  const Animal* j = new Dog();
+  const Animal* i = new Cat();
+
+  std::cout << std::endl;
+
+  std::cout << j->getType() << " " << std::endl;
+  std::cout << i->getType() << " " << std::endl;
+
+  i->makeSound(); //will output the cat sound!
+  j->makeSound();
+  meta->makeSound();
+
+  std::cout << "=== Desctrutor ===" << std::endl;
+  delete meta;
+  delete i;
+  delete j;
+
+  return (0);
   }
