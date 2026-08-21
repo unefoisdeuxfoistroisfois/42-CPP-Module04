fc65d6713ae42623f5beeecbf2c1a49f30a2dad4
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-08-21 21:48:30 +0200

    Implémentation de ICharacter

---
+++ ex03/ICharacter.hpp
@@ -0,0 +1,24 @@
+#ifndef ICHARACTER_HPP
+#define ICHARACTER_HPP
+
+#include "AMateria.hpp"
+#include <string.h>
+
+class AMateria;
+
+class ICharacter
+{
+	public:
+		// destructor
+		virtual ~ICharacter() {}
+		
+		// get set
+		virtual const std::string &getName() const = 0;
+
+		// fonctions 
+		virtual void equip(AMateria *m) = 0;
+		virtual void unequip(int idx) = 0;
+		virtual void use(int idx, ICharacter &target) = 0;
+};
+
+#endif
